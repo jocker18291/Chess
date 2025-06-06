a7c36d03dd9317416563d5445c76dec267b3765a
jocker18291 <dawidans714@gmail.com>
2025-06-06 14:02:33 +0200

    Add initialization function

--- src/board.cpp
+++ src/board.cpp
@@ -4,3 +4,22 @@
 Board::Board() {
     init();
 }
+
+void Board::init() {
+    std::string start[8] = {
+        "rnbqkbnr";
+        "pppppppp";
+        "........";
+        "........";
+        "........";
+        "........";
+        "PPPPPPPP";
+        "RNBQKBNR";
+    };
+
+    for(int i - 0; i < 8; i++) {
+        for(int j = 0; j < 8; j++) {
+            board[i][j] = start[i][j];
+        }
+    }
+}
