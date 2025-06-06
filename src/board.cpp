b03d0eea480dd28642c360ad33685384e84ea6f5
jocker18291 <dawidans714@gmail.com>
2025-06-06 23:11:42 +0200

    Initialized rook movement

--- src/board.cpp
+++ src/board.cpp
@@ -96,6 +96,30 @@ bool Board::makeMove(const std::string& move) {
             return false;
         }
     }
+
+    else if(tolower(piece) == 'r') {
+        if(!(dx == 0 || dy == 0)) {
+            return false;
+        }
+
+        int stepX = (dx == 0) ? 0 : (dx > 0 ? 1 : -1);
+        int stepY = (dy == 0) ? 0 : (dy > 0 ? 1 : -1);
+        int x = fromX + stepX;
+        int y = fromY + stepY;
+
+        while(x != toX || y != toY) {
+            if(board[y][x] != '.') {
+                return false;
+            }
+            x += stepX;
+            y += stepY;
+        }
+
+        if(board[toY][toX] != '.' && isupper(board[toY][toX] == isWhitePiece)) {
+            return false;
+        }
+    }
+
     else {
         return false;
     }
