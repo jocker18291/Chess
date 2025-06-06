b937d17c27763897df67ffe37621dcd48d2f59eb
jocker18291 <dawidans714@gmail.com>
2025-06-06 14:43:42 +0200

    Testing functions on main

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,13 @@ int main() {
     Board board;
     board.print();
 
+    std::string move;
+    std::cout << "Move: ";
+    std::cin >> move;
+    if(!board.makeMove(move)) {
+        std::cout << "Illegal!\n";
+    }
+    board.print();
+
     return 0;
 }
