3470b0c9a0f9e6664309d7c99f2abd1a97caa6e8
jocker18291 <dawidans714@gmail.com>
2025-06-07 22:49:20 +0200

    Rendering window

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,10 +1,21 @@
 #include <SFML/Graphics.hpp>
-#include "board.hpp"
-#include "ai.hpp"
+#include "../include/board.hpp"
+#include "../include/ai.hpp"
 #include <iostream>
 
 const int TILE_SIZE = 80;
 
 sf::Vector2i getTileFromMouse(int x, int y) {
     return {x / TILE_SIZE, y/TILE_SIZE};
+}
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(8 * TILE_SIZE, 8 * TILE_SIZE), "Chess");
+    Board board;
+
+    std::map<std::string, sf::Texture> textures;
+    std::map<char, std::string> figureMap = {
+        {'P', "w_piece"}, {'R', "rw_piece"}, {'N', "nw_piece"}, {'B', "bw_piece"}, {'Q', "qw_piece"}, {'K', "kw_piece"},
+        {'p', "piece"}, {'r', "r_piece"}, {'n', "n_piece"}, {'b', "b_piece"}, {'q', "q_piece"}, {'k', "k_piece"}
+    };
 }
