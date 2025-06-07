b79f82a8aee019dd9c9743d1afd4b566b619eeb0
jocker18291 <dawidans714@gmail.com>
2025-06-07 15:58:54 +0200

    Initialized minimax algorithm

--- src/ai.cpp
+++ src/ai.cpp
@@ -4,4 +4,30 @@ int minimax(Board board, int depth, bool maximizingPlayer) {
     if(depth == 0 || board.isGameOver()) {
         return board.evaluate();
     }
+
+    std::vector<Move> moves = board.getAllLegalMoves(board.isWhiteToMove());
+
+    if(moves.empty()) {
+        return board.evaluate();
+    }
+
+    if(maximizingPlayer) {
+        int best = -100000;
+        for(const Move& move : moves) {
+            Board newBoard = board;
+            newBoard.makeMove(move);
+            int score = minimax(newBoard, depth - 1, false);
+            best = std::max(best, score);
+        }
+        return best;
+    } else {
+        int best = 1000000;
+        for (const Move& move : moves) {
+            Board newBoard = board;
+            newBoard.makeMove(move);
+            int score = minimax(newBoard, depth - 1, true);
+            best = std::min(best, score);
+        }
+        return best;
+    }
 }
