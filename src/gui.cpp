533a1544e836568804709ca6fe62a6993def6fe4
jocker18291 <dawidans714@gmail.com>
2025-06-07 22:24:05 +0200

    Transfer mouse pixels into chessboard tiles

--- src/gui.cpp
+++ src/gui.cpp
@@ -2,3 +2,9 @@
 #include "board.hpp"
 #include "ai.hpp"
 #include <iostream>
+
+const int TILE_SIZE = 80;
+
+sf::Vector2i getTileFromMouse(int x, int y) {
+    return {x / TILE_SIZE, y/TILE_SIZE};
+}
