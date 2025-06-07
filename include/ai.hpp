00f36d141b6526d314f5b969faafb8978c9708b5
jocker18291 <dawidans714@gmail.com>
2025-06-07 16:18:33 +0200

    Fixed minimax parameters

--- include/ai.hpp
+++ include/ai.hpp
@@ -3,7 +3,7 @@
 
 #include "board.hpp"
 
-int minimax(Board board, int depth, bool maximizingPlayer);
+int minimax(Board board, int depth, bool maximizingPlayer, int alpha, int beta);
 Move findBestMove(Board board, int depth);
 
 #endif
